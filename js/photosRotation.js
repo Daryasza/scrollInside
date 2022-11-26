3faf576c1cd036876ff58fa05ffbf92903dafebb
Darya Sharnevich <darya.sharnevich@gmail.com>
2022-11-26 07:00:19 +0300

    feat: shadow under cursor added

--- js/photosRotation.js
+++ js/photosRotation.js
@@ -6,9 +6,13 @@ document.addEventListener('mousemove', function (e) {
     if (e.target === cardItem) {
       const cardHeightHalf = cardItem.offsetHeight / 2;
       const cardWidthHalf = cardItem.offsetWidth / 2;
+      const root = document.querySelector(':root');
+      const id = cardItem.id;
 
       cardItem.style.transform = `rotateY(${-(e.offsetX - cardWidthHalf)/4 }deg) rotateX(${(e.offsetY - cardHeightHalf)/4}deg)`;
-      cardItem.style.boxShadow = `inset ${e.offsetX}px ${e.offsetY}px 100px 10px rgba(0, 0, 0, 0.5)`
+      root.style.setProperty(`--shadow-opacity-${id}`, '1');
+      root.style.setProperty(`--shadow-positionX-${id}`, `${e.offsetY}px`);
+      root.style.setProperty(`--shadow-positionY-${id}`, `${e.offsetX}px`);
     }
   }
 });
@@ -17,9 +21,16 @@ document.addEventListener('mouseout', function(e) {
   for (let i = 0; i < cardItems.length; i++) {
     let cardItem = cardItems[i];
     if (e.target === cardItem) {
+      const root = document.querySelector(':root');
+      const id = cardItem.id;
+
+
       cardItem.style.transform = 'rotate(0deg)';
-      cardItem.style.boxShadow = 'none';
+      root.style.setProperty(`--shadow-opacity-${id}`, '0');
+      root.style.setProperty(`--shadow-positionX-${id}`, '0px');
+      root.style.setProperty(`--shadow-positionY-${id}`, '0px');
     }
   }
 });
+
 
